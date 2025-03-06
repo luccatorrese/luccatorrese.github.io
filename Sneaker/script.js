e3b178e0d4ce24eb8b7cfd2075ee6ef5df98457c
Lucca Torrese <123151223+luccatorrese@users.noreply.github.com>
2025-03-06 17:01:18 +0100

    Add files via upload

--- Sneaker/script.js
+++ Sneaker/script.js
@@ -1,10 +1,14 @@
 ﻿// Récupération de l'élément qui va changer de couleur
 const overlay = document.getElementById("product-shape");
-const overlay2 = document.getElementById("product-shape");
-const overlay3 = document.getElementById("product-shape");
 
-
+overlay.style.fill = "#be3d2c"
 function changeColor(picker) {
     // Set the fill style
     overlay.style.fill = picker.toHEXString();
+}
+
+const productsvg = document.getElementById('product-svg3')
+function changeColor(picker) {
+    productsvg.style.fill = picker.toHEXString()
+
 }

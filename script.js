45a79d9eb9f1650b0b3d37ba72c39677b442c05b
Lucca Torrese <123151223+luccatorrese@users.noreply.github.com>
2025-03-11 11:47:25 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,40 @@
-// script.js
-console.log("Le fichier JavaScript est correctement relié !");
+﻿// Récupération de l'élément qui va changer de couleur
+const overlay = document.getElementById("product-shape");
+
+function changeColor(picker) {
+    // Set the fill style
+    overlay.style.fill = picker.toHEXString();
+}
+
+const productsvg = document.getElementById('product-svg3')
+function changeColor(picker) {
+    productsvg.style.fill = picker.toHEXString()
+
+}
+
+const animation = bodymovin.loadAnimation({
+    container: document.getElementById('conteneur'),
+    path: 'Confetti_2.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+})
+animation.play();
+
+const lottieContainer = document.getElementById('conteneur');
+const animation = bodymovin.loadAnimation({
+    container: lottieContainer,
+    path: 'Confetti_2.json',
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+})
+
+// Play the animation on hover
+lottieContainer.addEventListener('mouseover', () => {
+    animation.play();
+});
+// Stop the animation after playing once
+animation.addEventListener('loopComplete', function () {
+    animation.stop();
+});
